6b786903f07dab7fe962e7ab00f09ff417d3eb7f
survasesupriya02-alt <survasesupriya02@gmail.com>
2026-01-05 14:37:50 +0530

    Add files via upload

---
+++ one.py
@@ -0,0 +1,10 @@
+def area_square(a):
+	print("square is:",a)
+def area_rect(l,b):
+	print("Area of rectangle is:",l,b)
+def area_circle(r):
+	r=int(input("Enter a radius"))
+	print("Area of circle",3.14*r*r)
+def area_tri(b,h):
+	print("Area of triangle is",0.5*b*h)
+	
---
+++ two.py
@@ -0,0 +1,5 @@
+import one
+one.area_square(8)
+one.area_rect(10,7)
+one.area_circle(6)
+one.area_tri(67,4)
